7f5dd035cc42d60589cce7d62dd772a714275f7d
WowDarkHole <isaacbartel.js@gmail.com>
2022-06-21 16:58:42 -0600

    Reponsive: mobile

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import Typewriter from 'typewriter-effect';
-import { Modal } from "react-responsive-modal";
-import TextTruncate from 'react-text-truncate';
 import { deposit, reverse, getDataAmount, check24hrs } from '../firebase/wallet';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -15,7 +12,6 @@ import "react-responsive-modal/styles.css";
 
 //Resources
 import Logo from '../src/img/logo.png';
-import BG from '../src/img/bg.png'
 
 export default function Home() {
 
@@ -39,18 +35,17 @@ export default function Home() {
   }
 
   const changeToggle = () => {
+    console.log("change")
     setToggle(!toggle);
   }
 
   const onConnectWallet = async () => {
-    let addressBump = '';
     if (window.ethereum) {
       await window.ethereum.request({ method: 'eth_requestAccounts' })
         .then(res => {
           // Return the address of the wallet
           setMetamask(true);
           setAddress(res[0]);
-          addressBump = res[0]
         })
     } else {
       notifyUnAvailable('Please install Metamask!')
@@ -87,48 +82,66 @@ export default function Home() {
 
   }
 
+  const truncate = (input) =>
+    input?.length > 15 ? `${input.substring(0, 10)}...` : input;
+
   return (
     <div className="mx-auto relative bg-[url('/img/bg.png')] bg-auto bg-cover bg-black overflow-hidden" >
       <Head>
         <title>METASCALE</title>
       </Head>
-      <nav className="border-gray-200 rounded dark:bg-gray-800 bg-transparent sticky z-20" role="navigation">
-        <div className="flex flex-wrap justify-between lg:justify-around items-center mx-auto bg-[#151515]">
-          <a href="#" className="flex">
-            <div className="text-5xl font-bold text-[#003949] mx-32">
-              <Image src={Logo} className="w-auto" width={50} height={50} />
+      <nav className="bg-gray-900">
+        <div className=" mx-auto px-2 sm:px-6 lg:px-8 py-3">
+          <div className="relative flex items-center justify-between h-16">
+            <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+              <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false" onClick={() => changeToggle()}>
+                <span className="sr-only">Open main menu</span>
+                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
             </div>
-          </a>
-          <div className="mx-3">
-            <span className="text-white text-4xl font-black">HACKTIVIST</span>
-          </div>
-          <div className={`${toggle ? '' : 'hidden'} justify-between items-center w-full md:flex md:w-auto md:order-1 mx-9`} id="mobile-menu-4">
-            <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 sm:text-xl md:text-xl lg:text-2xl items-center">
-
-              <li>
-                <button className="bg-[#003949] hover:bg-blue-700 text-white py-1 px-2 rounded-full text-base" onClick={() => depositToWallet()}> Staking </button>
-              </li>
-              <div className="z-50">
-                <button className="bg-[#003949] hover:bg-blue-700 text-white py-1 px-2 rounded-full text-base" onClick={() => onConnectWallet()}>
-                  <TextTruncate line={1} element="span" truncateText="..." text={address} />
-                </button>
-                <button data-collapse-toggle="mobile-menu-4" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-4" aria-expanded="false" onClick={() => changeToggle()}>
-                  <span className="sr-only pixel-font">Open main menu</span>
-                  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                  <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                </button>
+            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-between">
+              <div className="flex-shrink-0 flex items-center">
+                <Image src={Logo} className="block lg:hidden w-auto" width={50} height={50} alt="Hactivist" />
               </div>
-              <li className="text-white text-sm">
-                <span>{dataAmount} </span>
-                <span>$Data</span>
-              </li>
-            </ul>
+              <div className="hidden lg:block w-auto flex-shrink-0 flex items-center">
+                <a className="text-white text-4xl font-black" alt="Hactivist">HACKTIVIST</a>
+              </div>
+              <div className="hidden sm:block sm:ml-6">
+                <div className="flex space-x-4">
+                  <a href="#" className="text-gray-300 bg-gray-800  hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-xl font-medium" onClick={() => depositToWallet()}>Staking
+                  </a>
+                  <a href="#" className="text-gray-300 bg-gray-800 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-xl font-medium" onClick={() => onConnectWallet()}>
+                    {truncate(address)}
+                  </a>
+                  <span className="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-xl font-medium">
+                    <span>{dataAmount} </span>
+                    <span>$Data</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+        <div className={`${toggle ? 'sm:hidden' : 'hidden sm:hidden'}`} id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 text-right">
+            <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              <span>{dataAmount} </span>
+              <span>$Data</span>
+            </span>
+            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium" onClick={() => depositToWallet()}>Staking</a>
+            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium" onClick={() => onConnectWallet()}>
+              {truncate(address)}
+            </a>
           </div>
         </div>
       </nav >
-      {/* <div className="bg-fixed" >
-        <Image src={BG}></Image>
-      </div> */}
       <div className="h-screen w-screen">
 
       </div>
